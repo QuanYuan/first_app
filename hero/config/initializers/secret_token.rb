8777dff933c4d37d68dafa2b4a9caccd5be4a171
QuanYuan <quanyuan@seas.upenn.edu>
2013-03-07 18:37:26 -0500

    1

---
+++ hero/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hero::Application.config.secret_token = '2a2e39ad205576ca788bc4c7e679c6f07bfd07e57c4207cf3642867d1dd06d61d52038f538cf6533205267c6ee5d3ea3a35feab1cdfcaa057ea9ca921b73f860'
