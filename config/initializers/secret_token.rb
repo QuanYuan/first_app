5cea855461b8c81d38bd52c442b58caed062eb08
QuanYuan <quanyuan@seas.upenn.edu>
2013-03-07 18:24:59 -0500

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testheroku::Application.config.secret_token = 'e161e47e2860dd4f9768375e50d1c055d84feac36eb4c0a7116f7cc2f409215b3c63a32d2a7831464a8122f55cc8123b83c9b4e6c31c92ec64ce260c456d1def'
